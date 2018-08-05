77316d6e60ea2bd4bb9474e8ac865d6c981abeb8
hikalium <hikalium@hikalium.com>
2018-08-05 15:13:16 +0900

    Remove files not used

--- src/efimain.c
+++ src/efimain.c
@@ -1,10 +1,19 @@
-#include <efi.h>
-#include <common.h>
+#include "efi.h"
+struct EFI_SYSTEM_TABLE *_system_table;
 
-void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *SystemTable)
+void EFIInit(struct EFI_SYSTEM_TABLE *system_table)
 {
-	efi_init(SystemTable);
-	puts(L"Hello, liumOS world!...\r\n");
-	while (TRUE);
+	_system_table = system_table;
 }
 
+void EFIPutString(unsigned short *s)
+{
+	_system_table->ConOut->OutputString(_system_table->ConOut, s);
+}
+
+void efi_main(void *ImageHandle, struct EFI_SYSTEM_TABLE *system_table) {
+  EFIInit(system_table);
+  EFIPutString(L"Hello, liumOS world!...\r\n");
+  while (1) {
+  };
+}
--- src/include/gui.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GUI_H_
-#define _GUI_H_
-
-void gui(void);
-
-#endif
