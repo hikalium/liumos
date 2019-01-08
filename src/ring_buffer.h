fc80fd1fd068e72748bde57323e79bde70290af3
hikalium <hikalium@hikalium.com>
2019-01-08 14:24:51 +0900

    Impl ring buffer. Close #2.

---
+++ src/ring_buffer.h
@@ -0,0 +1,26 @@
+#include "liumos.h"
+
+template <typename T, size_t n>
+class RingBuffer {
+ public:
+  RingBuffer() : readp_(0), writep_(0) {}
+  T Pop(void) {
+    if (readp_ == writep_)
+      return 0;
+    T v = elements_[readp_];
+    readp_ = (readp_ + 1) % n;
+    return v;
+  }
+  void Push(T value) {
+    int nextp = (writep_ + 1) % n;
+    if (nextp == readp_)
+      return;
+    elements_[writep_] = value;
+    writep_ = nextp;
+  }
+
+ private:
+  T elements_[n];
+  int readp_;
+  int writep_;
+};
---
+++ src/ring_buffer_test.cc
@@ -0,0 +1,27 @@
+#include "ring_buffer.h"
+
+#ifdef LIUMOS_TEST
+
+#include <assert.h>
+#include <stdio.h>
+
+int main() {
+  RingBuffer<int, 4> rbuf;
+
+  rbuf.Push(3);
+  rbuf.Push(5);
+  rbuf.Push(7);
+  rbuf.Push(11);
+  rbuf.Push(13);
+  assert(rbuf.Pop() == 3);
+  rbuf.Push(17);
+  assert(rbuf.Pop() == 5);
+  assert(rbuf.Pop() == 7);
+  assert(rbuf.Pop() == 17);
+  assert(rbuf.Pop() == 0);
+
+  puts("PASS");
+  return 0;
+}
+
+#endif
