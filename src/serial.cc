b3864243786f3b48cc9e8e364045d3d42a971a3b
hikalium <hikalium@hikalium.com>
2019-02-20 13:22:21 +0900

    Change serial baud-rate to divisor 1 ( 115200 )

--- src/serial.cc
+++ src/serial.cc
@@ -4,7 +4,7 @@ void SerialPort::Init(uint16_t port) {
   port_ = port;
   WriteIOPort8(port_ + 1, 0x00);  // Disable all interrupts
   WriteIOPort8(port_ + 3, 0x80);  // Enable DLAB (set baud rate divisor)
-  WriteIOPort8(port_ + 0, 0x03);  // Set divisor to 3 (lo byte) 38400 baud
+  WriteIOPort8(port_ + 0, 0x01);  // Set divisor (lo byte) of baud rate
   WriteIOPort8(port_ + 1, 0x00);  //                  (hi byte)
   WriteIOPort8(port_ + 3, 0x03);  // 8 bits, no parity, one stop bit
   WriteIOPort8(port_ + 2,
