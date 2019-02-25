84a594607e9b13e5fb14f0f3fd8c65a2010ea3de
hikalium <hikalium@hikalium.com>
2019-02-25 09:32:55 +0900

    Add phys_page_allocator.h

--- src/liumos.h
+++ src/liumos.h
@@ -11,6 +11,7 @@
 #include "interrupt.h"
 #include "keyid.h"
 #include "paging.h"
+#include "phys_page_allocator.h"
 #include "serial.h"
 #include "sheet.h"
 #include "text_box.h"
@@ -89,8 +90,6 @@ inline uint64_t ByteSizeToPageSize(uint64_t byte_size) {
   return (byte_size + kPageSize - 1) >> kPageSizeExponent;
 }
 
-class PhysicalPageAllocator;
-
 extern EFI::MemoryMap efi_memory_map;
 extern PhysicalPageAllocator* page_allocator;
 extern int kMaxPhyAddr;
@@ -102,43 +101,3 @@ extern File liumos_elf_file;
 extern HPET hpet;
 
 void MainForBootProcessor(void* image_handle, EFI::SystemTable* system_table);
-
-// @phys_page_allocator.cc
-class PhysicalPageAllocator {
- public:
-  PhysicalPageAllocator() : head_(nullptr) {}
-  void FreePages(void* phys_addr, uint64_t num_of_pages);
-  template <typename T>
-  T AllocPages(int num_of_pages) {
-    FreeInfo* info = head_;
-    void* addr = nullptr;
-    while (info) {
-      addr = info->ProvidePages(num_of_pages);
-      if (addr)
-        return reinterpret_cast<T>(addr);
-      info = info->GetNext();
-    }
-    Panic("Cannot allocate pages");
-  }
-  void Print();
-
- private:
-  class FreeInfo {
-   public:
-    FreeInfo(uint64_t num_of_pages, FreeInfo* next)
-        : num_of_pages_(num_of_pages), next_(next) {}
-    FreeInfo* GetNext() const { return next_; }
-    void* ProvidePages(int num_of_req_pages);
-    void Print();
-
-   private:
-    bool CanProvidePages(uint64_t num_of_req_pages) const {
-      return num_of_req_pages < num_of_pages_;
-    }
-
-    uint64_t num_of_pages_;
-    FreeInfo* next_;
-  };
-
-  FreeInfo* head_;
-};
---
+++ src/phys_page_allocator.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "generic.h"
+
+class PhysicalPageAllocator {
+ public:
+  PhysicalPageAllocator() : head_(nullptr) {}
+  void FreePages(void* phys_addr, uint64_t num_of_pages);
+  template <typename T>
+  T AllocPages(int num_of_pages) {
+    FreeInfo* info = head_;
+    void* addr = nullptr;
+    while (info) {
+      addr = info->ProvidePages(num_of_pages);
+      if (addr)
+        return reinterpret_cast<T>(addr);
+      info = info->GetNext();
+    }
+    Panic("Cannot allocate pages");
+  }
+  void Print();
+
+ private:
+  class FreeInfo {
+   public:
+    FreeInfo(uint64_t num_of_pages, FreeInfo* next)
+        : num_of_pages_(num_of_pages), next_(next) {}
+    FreeInfo* GetNext() const { return next_; }
+    void* ProvidePages(int num_of_req_pages);
+    void Print();
+
+   private:
+    bool CanProvidePages(uint64_t num_of_req_pages) const {
+      return num_of_req_pages < num_of_pages_;
+    }
+
+    uint64_t num_of_pages_;
+    FreeInfo* next_;
+  };
+
+  FreeInfo* head_;
+};
