f78b68d46da52c3f2b57a98735f1c23b5194d7e4
hikalium <hikalium@hikalium.com>
2019-03-21 00:11:14 +0900

    Add subtask.cc

--- src/kernel.cc
+++ src/kernel.cc
@@ -1,65 +1,6 @@
 #include "liumos.h"
 
-int counter;
-
 LiumOS* liumos;
-
-void SubTask() {
-  constexpr int map_ysize_shift = 4;
-  constexpr int map_xsize_shift = 5;
-  constexpr int pixel_size = 8;
-
-  constexpr int ysize_mask = (1 << map_ysize_shift) - 1;
-  constexpr int xsize_mask = (1 << map_xsize_shift) - 1;
-  constexpr int ysize = 1 << map_ysize_shift;
-  constexpr int xsize = 1 << map_xsize_shift;
-  static char map[ysize * xsize];
-  constexpr int canvas_ysize = ysize * pixel_size;
-  constexpr int canvas_xsize = xsize * pixel_size;
-
-  map[(ysize / 2 - 1) * xsize + (xsize / 2 - 3)] = 1;
-  map[(ysize / 2 - 1) * xsize + (xsize / 2 + 2)] = 1;
-
-  map[(ysize / 2) * xsize + (xsize / 2 - 4)] = 1;
-  map[(ysize / 2) * xsize + (xsize / 2 - 3)] = 1;
-  map[(ysize / 2) * xsize + (xsize / 2 + 2)] = 1;
-  map[(ysize / 2) * xsize + (xsize / 2 + 3)] = 1;
-
-  map[(ysize / 2 + 1) * xsize + (xsize / 2 - 3)] = 1;
-  map[(ysize / 2 + 1) * xsize + (xsize / 2 + 2)] = 1;
-
-  while (1) {
-    for (int y = 0; y < ysize; y++) {
-      for (int x = 0; x < xsize; x++) {
-        int count = 0;
-        for (int p = -1; p <= 1; p++)
-          for (int q = -1; q <= 1; q++)
-            count +=
-                map[((y + p) & ysize_mask) * xsize + ((x + q) & xsize_mask)] &
-                1;
-        count -= map[y * xsize + x] & 1;
-        if ((map[y * xsize + x] && (count == 2 || count == 3)) ||
-            (!map[y * xsize + x] && count == 3))
-          map[y * xsize + x] |= 2;
-      }
-    }
-    for (int y = 0; y < ysize; y++) {
-      for (int x = 0; x < xsize; x++) {
-        int p = map[y * xsize + x];
-        int col = 0x000000;
-        if (p & 1)
-          col = 0xff0088 * (p & 2) + 0x00cc00 * (p & 1);
-        map[y * xsize + x] >>= 1;
-        liumos->screen_sheet->DrawRectWithoutFlush(
-            liumos->screen_sheet->GetXSize() - canvas_xsize + x * pixel_size,
-            y * pixel_size, pixel_size, pixel_size, col);
-      }
-    }
-    liumos->screen_sheet->Flush(liumos->screen_sheet->GetXSize() - canvas_xsize,
-                                0, canvas_xsize, canvas_ysize);
-    liumos->hpet->BusyWait(200);
-  }
-}
 
 GDT gdt_;
 IDT idt_;
@@ -96,6 +37,8 @@ void InitializeVRAMForKernel() {
   liumos->screen_sheet = &virtual_screen_;
   liumos->screen_sheet->DrawRect(0, 0, xsize, ysize, 0xc6c6c6);
 }
+
+void SubTask();  // @subtask.cc
 
 void LaunchSubTask(KernelVirtualHeapAllocator& kernel_heap_allocator) {
   const int kNumOfStackPages = 3;
