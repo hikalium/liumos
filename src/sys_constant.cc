695193475c1851e269b9dcefa710729ca64e002e
hikalium <hikalium@hikalium.com>
2019-04-24 23:47:00 +0900

    Improve githash generation. Close #24

--- src/sys_constant.cc
+++ src/sys_constant.cc
@@ -4,7 +4,6 @@ uint64_t GetPhysAddrMask() {
   return liumos->cpu_features->phy_addr_mask;
 }
 
-static const char* kVersionStr = GIT_HASH;
 const char* GetVersionStr() {
-  return kVersionStr;
+  return kGitHash;
 }
