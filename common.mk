ac4f40893f6edadc6c771d7ef8b97dcb21364ee4
hikalium <hikalium@hikalium.com>
2019-05-10 19:51:19 +0900

    Fix Makefile for CI

--- common.mk
+++ common.mk
@@ -2,15 +2,19 @@ include cc_cache.gen.mk
 OSNAME=${shell uname -s}
 ifeq ($(OSNAME),Darwin)
 cc_cache.gen.mk : Makefile
-	@LLVM_PREFIX=`brew --prefix llvm` && \
-		echo "CC:=$$LLVM_PREFIX/bin/clang" > $@ && \
-		echo "LLD_LINK:=$$LLVM_PREFIX/bin/lld-link" >> $@ && \
-		echo "LD_LLD:=$$LLVM_PREFIX/bin/ld.lld" >> $@
+	@ echo "" > $@ && \
+		LLVM_PREFIX=`brew --prefix llvm` && \
+		echo "LLVM_CC:=$$LLVM_PREFIX/bin/clang" >> $@ && \
+		echo "LLVM_CXX:=$$LLVM_PREFIX/bin/clang++" >> $@ && \
+		echo "LLVM_LLD_LINK:=$$LLVM_PREFIX/bin/lld-link" >> $@ && \
+		echo "LLVM_LD_LLD:=$$LLVM_PREFIX/bin/ld.lld" >> $@
 	cat $@
 else
 cc_cache.gen.mk : Makefile
-	echo "CC:=`which clang`" > $@ && \
-		echo "LLD_LINK:=`which lld-link-4.0`" >> $@
+	@ echo "" > $@ && \
+		echo "LLVM_CC:=`which clang`" >> $@ && \
+		echo "LLVM_CXX:=`which clang++`" >> $@ && \
+		echo "LLVM_LLD_LINK:=`which lld-link-4.0`" >> $@
 	cat $@
 endif
 
