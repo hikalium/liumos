9e478a4c91e8b5a37f569204767f00a328e545f8
hikalium <hikalium@hikalium.com>
2019-07-21 20:16:34 +0900

    Remove unused stabs in libcxx_support.cc

--- src/libcxx_support.cc
+++ src/libcxx_support.cc
@@ -7,8 +7,3 @@ void* operator new(unsigned long, std::align_val_t) {
 void operator delete(void*, std::align_val_t) {
   Panic("void operator delete(void*, std::align_val_t)");
 }
-
-void* __eh_frame_end;
-void* __eh_frame_start;
-void* __eh_frame_hdr_end;
-void* __eh_frame_hdr_start;
