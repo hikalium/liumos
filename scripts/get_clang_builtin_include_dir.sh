1df7299f42339a60eb2d8362a898b12bf6e1367a
hikalium <hikalium@hikalium.com>
2019-12-19 13:34:23 +0900

    Update scripts/get_clang_builtin_include_dir.sh

--- scripts/get_clang_builtin_include_dir.sh
+++ scripts/get_clang_builtin_include_dir.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-if [ "$(uname)" == 'Darwin' ]; then
-	# macOS
-	version=`/usr/local/opt/llvm/bin/clang --version | head -1 | sed 's/^.*[^0-9] \([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
-	basepath="$(dirname $(dirname /usr/local/opt/llvm/bin/clang))"
-	echo ${basepath}/lib/clang/${version}/include
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-	# Linux
+if [ "$(uname)" == 'Darwin' ] || [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+	# macOS, Linux
 	version=`$1 --version | head -1 | sed 's/^.*[^0-9] \([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 	basepath="$(dirname $(dirname $(which $1)))"
 	echo ${basepath}/lib/clang/${version}/include
