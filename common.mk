5145b1cac3f08196b4da50d84c1ea1099d1a4bec
hikalium <hikalium@hikalium.com>
2020-08-26 23:12:31 +0900

    Support parallel build on macOS

--- common.mk
+++ common.mk
@@ -3,7 +3,7 @@ THIS_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 OSNAME=${shell uname -s}
 
 ifeq ($(OSNAME),Darwin)
-$(THIS_DIR)cc_cache.gen.mk : $(THIS_DIR)scripts/gen_tool_defs_linux.sh
+$(THIS_DIR)cc_cache.gen.mk : $(THIS_DIR)scripts/gen_tool_defs_macos.sh
 	@ $(THIS_DIR)scripts/gen_tool_defs_macos.sh > $@
 else
 $(THIS_DIR)cc_cache.gen.mk : $(THIS_DIR)scripts/gen_tool_defs_linux.sh
@@ -12,7 +12,8 @@ endif
 
 include $(THIS_DIR)cc_cache.gen.mk
 CLANG_SYSTEM_INC_PATH=$(shell $(THIS_DIR)./scripts/get_clang_builtin_include_dir.sh $(LLVM_CXX))
+LIUM_NCPU?=1
 
 dump_config : 
-	@ echo LLVM_CC=$(LLVM_CC)
+	@ cat $(THIS_DIR)cc_cache.gen.mk
 	@ echo CLANG_SYSTEM_INC_PATH=$(CLANG_SYSTEM_INC_PATH)
