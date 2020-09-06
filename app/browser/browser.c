f9600bd26f15ebd194d35d5e76877650e25b9733
Asami Doi <doiasami1219@gmail.com>
2020-09-06 16:28:43 +0900

    Cleanup.

--- app/browser/browser.c
+++ app/browser/browser.c
@@ -1,8 +1,6 @@
 #include "browser.h"
 #include "rendering.h"
 
-//#include <stdio.h>
-
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     write(1, "usage: browser <rawtext>\n", 25);
@@ -12,17 +10,6 @@ int main(int argc, char *argv[]) {
 
   char* html = argv[1];
   render(html);
-
-  /*
-  char output[1000];
-  int i = 0;
-  while (md[i] != '\0') {
-    output[i] = md[i];
-    i++;
-  }
-  output[i] = '\n';
-  //write(1, output, i+1);
-  */
 
   exit(0);
   return 0;
