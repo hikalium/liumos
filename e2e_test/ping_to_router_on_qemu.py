f4668375050bd196254d6f29d780f5061d8a8510
hikalium <hikalium@hikalium.com>
2020-12-03 21:22:07 +0900

    Add e2e_test/test_util.py

--- e2e_test/ping_to_router_on_qemu.py
+++ e2e_test/ping_to_router_on_qemu.py
@@ -1,57 +1,13 @@
 #!/usr/bin/env python3
-import pexpect
 import time
-import itertools
-import csv
-import random
-import os
-import platform
-import datetime
 import sys
-import json
-import parse
-import argparse
-
-liumos_root_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-print('liumOS root is at: ', liumos_root_path, file=sys.stderr, flush=True)
-
-def launch_qemu():
-    p = pexpect.spawn("make -C {} run_for_e2e_test".format(liumos_root_path))
-    p.expect(r"(qemu)")
-    print('QEMU Launched', file=sys.stderr, flush=True)
-    return p
-
-def connect_to_liumos_serial():
-    p = pexpect.spawn("telnet localhost 1235")
-    p.expect(r"(liumos)")
-    print('Reached to liumos prompt via serial', file=sys.stderr, flush=True)
-    return p
-
-def just_print_everything(p): # For debugging
-    while True:
-        try:
-            print(p.readline())
-        except pexpect.EOF:
-            print("Got EOF.")
-            print(p.before)
-            sys.exit(1)
-
-def expect_liumos_command_result(p, cmd, expected, timeout):
-    p.sendline(cmd);
-    try:
-        p.expect(expected, timeout=timeout)
-    except pexpect.TIMEOUT:
-        print("FAIL (timed out): {} => {}".format(cmd, expected))
-        print(p.before)
-        sys.exit(1)
-    print("PASS: {} => {}".format(cmd, expected))
-
+import test_util
 
 if __name__ == "__main__":
-    qemu_mon_conn = launch_qemu()
+    qemu_mon_conn = test_util.launch_qemu()
     time.sleep(1)
-    liumos_serial_conn = connect_to_liumos_serial()
+    liumos_serial_conn = test_util.connect_to_liumos_serial()
     time.sleep(1)
-    expect_liumos_command_result(liumos_serial_conn, "ping.bin 10.0.2.2", "ICMP packet recieved from 10.0.2.2 ICMP Type = 0", 5)
+    test_util.expect_liumos_command_result(liumos_serial_conn, "ping.bin 10.0.2.2", "ICMP packet recieved from 10.0.2.2 ICMP Type = 0", 5)
     sys.exit(0)
 
---
+++ e2e_test/test_util.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import pexpect
+import os
+import sys
+
+liumos_root_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+print('liumOS root is at: ', liumos_root_path, file=sys.stderr, flush=True)
+
+def launch_qemu():
+    p = pexpect.spawn("make -C {} run_for_e2e_test".format(liumos_root_path))
+    p.expect(r"(qemu)")
+    print('QEMU Launched', file=sys.stderr, flush=True)
+    return p
+
+def connect_to_liumos_serial():
+    p = pexpect.spawn("telnet localhost 1235")
+    p.expect(r"(liumos)")
+    print('Reached to liumos prompt via serial', file=sys.stderr, flush=True)
+    return p
+
+def just_print_everything(p): # For debugging
+    while True:
+        try:
+            print(p.readline())
+        except pexpect.EOF:
+            print("Got EOF.")
+            print(p.before)
+            sys.exit(1)
+
+def expect_liumos_command_result(p, cmd, expected, timeout):
+    p.sendline(cmd);
+    try:
+        p.expect(expected, timeout=timeout)
+    except pexpect.TIMEOUT:
+        print("FAIL (timed out): {} => {}".format(cmd, expected))
+        print(p.before)
+        sys.exit(1)
+    print("PASS: {} => {}".format(cmd, expected))
+
