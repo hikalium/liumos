ba8ad81b968bd726822a64f4e0a4da77fac73269
hikalium <hikalium@hikalium.com>
2021-01-05 14:22:49 +0900

    Use archive file instead of object files

--- app/guitest/build_freetype.sh
+++ app/guitest/build_freetype.sh
@@ -10,9 +10,11 @@ export CFLAGS="-fno-stack-protector \
 	-g -std=c99 --target=x86_64-pc-linux-elf  \
 	-fuse-ld=/usr/local/opt/llvm/bin/ld.lld  \
 	-nostdlib -I${TOP_DIR}/freetype_support/include/"
-export CC='/usr/local/opt/llvm/bin/clang-11 -v'
-export LLD='/usr/local/opt/llvm/bin/ld.lld -v'
-export LD='/usr/local/opt/llvm/bin/ld.lld -v'
+export CC='/usr/local/opt/llvm/bin/clang-11'
+export LLD='/usr/local/opt/llvm/bin/ld.lld'
+export LD='/usr/local/opt/llvm/bin/ld.lld'
+export AR='/usr/local/opt/llvm/bin/llvm-ar'
+export RANLIB='/usr/local/opt/llvm/bin/llvm-ranlib'
 ./configure \
 	--prefix="${TOP_DIR}/freetype_root" \
 	--build=x86_64-pc-linux-gnu \
@@ -20,4 +22,5 @@ export LD='/usr/local/opt/llvm/bin/ld.lld -v'
 	--enable-shared=no \
 	--without-zlib
 make
+make install
 
