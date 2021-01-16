e0323f33f09ba4d4f91471a279f4b62ec46adf5b
hikalium <hikalium@hikalium.com>
2021-01-16 22:09:55 +0900

    Fix unit tests on mac (use host compiler for unit tests)

--- scripts/gen_tool_defs_macos.sh
+++ scripts/gen_tool_defs_macos.sh
@@ -1,6 +1,7 @@
 LLVM_PREFIX=`brew --prefix llvm`
 echo "LLVM_CC:=${LLVM_PREFIX}/bin/clang"
 echo "LLVM_CXX:=${LLVM_PREFIX}/bin/clang++"
+echo "HOST_CXX:=clang++"
 echo "LLVM_LLD_LINK:=${LLVM_PREFIX}/bin/lld-link"
 echo "LLVM_LD_LLD:=${LLVM_PREFIX}/bin/ld.lld"
 echo "LLVM_AR:=${LLVM_PREFIX}/bin/llvm-ar"
