56f1a7782c9dd0aec188b5b8b97c72c05f66ad48
hikalium <hikalium@hikalium.com>
2021-04-25 14:56:01 +0900

    Add StdIoWriter

--- app/shelium/src/main.rs
+++ app/shelium/src/main.rs
@@ -2,8 +2,10 @@
 #![no_main]
 
 use core::convert::TryInto;
+use core::fmt;
 use core::mem::size_of;
 use core::panic::PanicInfo;
+use core::str;
 
 #[link(name = "liumos", kind = "static")]
 extern "C" {
@@ -24,7 +26,7 @@ fn getchar() -> u8 {
     }
     c
 }
-fn print(s: &str) {
+fn print_string(s: &str) {
     unsafe {
         sys_write(1, s.as_ptr(), s.len().try_into().unwrap());
     }
@@ -40,14 +42,51 @@ fn exit(code: i32) -> ! {
     }
 }
 
+pub struct StdIoWriter {}
+impl StdIoWriter {}
+impl fmt::Write for StdIoWriter {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        print_string(s);
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+        ($($arg:tt)*) => (_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+        () => ($crate::print!("\n"));
+            ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    let mut writer = crate::StdIoWriter {};
+    fmt::write(&mut writer, args).unwrap();
+}
+
 fn main() {
-    let s = "Hello, Rust world on liumOS!\n";
-    print(s);
+    println!("Welcome to shelium, a simple shell for liumOS written in Rust!");
+    let mut buf: [u8; 32] = [0; 32];
+    let mut buf_used = 0;
     loop {
         let c = getchar();
         putchar(c);
         if c == 'q' as u8 {
             break;
+        }
+        if buf_used < buf.len() - 1 {
+            buf[buf_used] = c;
+            buf_used += 1;
+            buf[buf_used] = 0;
+        }
+        if c == '\n' as u8 {
+            let s = str::from_utf8(&buf).unwrap();
+            println!("{:?}", s);
+            buf_used = 0;
         }
     }
 }
