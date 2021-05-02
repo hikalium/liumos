1c41b7c5eb757eb0a0838a482fd04aa9b8ceb3a4
hikalium <hikalium@hikalium.com>
2021-05-03 00:03:27 +0900

    Update ls

--- app/liumlib/src/lib.rs
+++ app/liumlib/src/lib.rs
@@ -112,7 +112,7 @@ trait MutableAllocator {
     fn dealloc(&mut self, _ptr: *mut u8, _layout: Layout);
 }
 
-const ALLOCATOR_BUF_SIZE: usize = 0x1000;
+const ALLOCATOR_BUF_SIZE: usize = 0x2000;
 pub struct WaterMarkAllocator {
     buf: [u8; ALLOCATOR_BUF_SIZE],
     used_bytes: usize,
--- app/shelium/src/main.rs
+++ app/shelium/src/main.rs
@@ -17,34 +17,46 @@ fn run_command(line: &str) {
         return;
     }
     if line == "ls" {
-        let fd = open("/", 0, 0);
+        let fd = open(".", 0, 0);
         if fd < 0 {
             println!("open failed: {}", fd);
             return;
         }
-        let mut buf = [0; 1024];
+        let mut buf = [0; 512];
         let p = buf.as_mut_ptr();
-        let len = buf.len();
-        let bytes_read = unsafe { sys_getdents64(fd as u32, p, len) };
-        if bytes_read < 0 {
-            println!("getdents failed");
-            return;
-        }
-        let mut cur = 0;
         let mut files = Vec::new();
         struct FileNameAndId {
             name: String,
             id: u64,
         }
-        while cur < bytes_read as usize {
-            let de: &DirectoryEntry = unsafe { &*(p.add(cur) as *mut DirectoryEntry) };
-            let file_name_bytes = &buf[(size_of::<DirectoryEntry>() + cur)..(de.size() + cur)];
-            let file_name = String::from_utf8(file_name_bytes.to_vec()).unwrap();
-            cur += de.size();
-            files.push(FileNameAndId {
-                name: file_name,
-                id: de.inode(),
-            })
+        loop {
+            let bytes_read = unsafe { sys_getdents64(fd as u32, p, buf.len()) };
+            if bytes_read < 0 {
+                println!("getdents failed");
+                return;
+            }
+            if bytes_read == 0 {
+                break;
+            }
+            let mut cur = 0;
+            while cur < bytes_read as usize {
+                let de: &DirectoryEntry = unsafe { &*(p.add(cur) as *mut DirectoryEntry) };
+                let file_name_bytes = &buf[(size_of::<DirectoryEntry>() + cur)..(de.size() + cur)];
+                let null_terminator_pos = file_name_bytes.iter().position(|&v| v == 0);
+                let file_name_bytes = match null_terminator_pos {
+                    Some(idx) => &file_name_bytes[..idx],
+                    None => file_name_bytes,
+                };
+                let file_name = String::from_utf8(file_name_bytes.to_vec()).unwrap();
+                cur += de.size();
+                if file_name.starts_with('.') {
+                    continue;
+                }
+                files.push(FileNameAndId {
+                    name: file_name,
+                    id: de.inode(),
+                })
+            }
         }
         files.sort_by(|l, r| l.name.cmp(&r.name));
         for f in files {
@@ -62,7 +74,7 @@ fn main() {
         putchar(c);
         if c == b'\n' {
             println!("{:?}", line);
-            if line == "q" {
+            if line == "exit" {
                 break;
             }
             run_command(&line);
