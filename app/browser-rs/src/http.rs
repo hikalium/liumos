cc0ff3da788bde9f182e168ad622cf1325f2cbc7
Asami Doi <doiasami1219@gmail.com>
2021-05-30 13:53:18 +0900

    browser-rs: Add Host header to HTTP request

--- app/browser-rs/src/http.rs
+++ app/browser-rs/src/http.rs
@@ -22,6 +22,12 @@ struct Header {
     value: String,
 }
 
+impl Header {
+    fn new(key: String, value: String) -> Self {
+        Self { key, value }
+    }
+}
+
 #[derive(Debug)]
 pub struct HTTPRequest {
     method: Method,
@@ -33,24 +39,44 @@ pub struct HTTPRequest {
 
 impl HTTPRequest {
     pub fn new(method: Method, url: &ParsedUrl) -> Self {
-        Self {
+        let mut req = Self {
             method,
             path: String::from(&url.path),
             version: String::from("HTTP/1.1"),
             headers: Vec::new(),
             body: String::from("Hello world"),
-        }
+        };
+
+        req.add_header(String::from("Host"), String::from(&url.host));
+
+        req
+    }
+
+    pub fn add_header(&mut self, key: String, value: String) {
+        self.headers.push(Header::new(key, value));
     }
 
     pub fn string(&self) -> String {
-        let mut request = String::from(self.method.name());
+        // request line
+        let mut request = self.method.name();
         request.push(' ');
         request.push_str(&self.path);
         request.push(' ');
         request.push_str(&self.version);
         request.push('\n');
+
+        // headers
+        for h in &self.headers {
+            request.push_str(&h.key);
+            request.push_str(": ");
+            request.push_str(&h.value);
+            request.push('\n');
+        }
         request.push('\n');
+
+        // body
         request.push_str(&self.body);
+
         request
     }
 }
