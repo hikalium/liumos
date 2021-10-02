5c6936e624839742900dc98829c5bb562ae8bfdb
Asami Doi <doiasami1219@gmail.com>
2021-10-02 17:38:37 +0900

    browser-rs: add a default page

--- app/browser-rs/src/main.rs
+++ app/browser-rs/src/main.rs
@@ -21,12 +21,14 @@ fn help_message() {
     println!("Usage: browser-rs.bin [ OPTIONS ]");
     println!("       -u, --url      URL. Default: http://127.0.0.1:8888/index.html");
     println!("       -h, --help     Show this help message.");
+    println!("       -d, --default  Show a default page with embedded HTML content for test.");
     exit(0);
 }
 
 entry_point!(main);
 fn main() {
     let mut url = "http://127.0.0.1:8888/index.html";
+    let mut default = false;
 
     let args = env::args();
     for i in 1..args.len() {
@@ -41,17 +43,44 @@ fn main() {
             }
             url = args[i + 1];
         }
-    }
 
-    let parsed_url = ParsedUrl::new(url.to_string());
+        if "--default".to_string() == args[i] || "-d".to_string() == args[i] {
+            default = true;
+        }
+    }
 
     let mut app = ApplicationWindow::new(512, 256, "my browser".to_string());
     app.initialize();
 
-    let response = udp_response(&parsed_url);
+    if default {
+        let default_page = r#"
+<html>
+  <head>
+    <style>
+    h1 {
+        background-color:red;
+    }
+    </style>
+    </head>
+  <body>
+    Hello, this is a toy browser.
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
+  </body>
+</html>"#;
 
-    println!("----- receiving a response -----");
-    println!("{}", response);
+        render(default_page.to_string(), &app);
+    } else {
+        let parsed_url = ParsedUrl::new(url.to_string());
+        println!("parsed_url: {:?}", parsed_url);
 
-    render(response);
+        println!("----- receiving a response -----");
+        let response = udp_response(&parsed_url);
+        println!("{}", response);
+
+        render(response, &app);
+    }
 }
--- app/browser-rs/src/renderer.rs
+++ app/browser-rs/src/renderer.rs
@@ -2,15 +2,15 @@ pub mod css;
 pub mod dom;
 pub mod tokenizer;
 
+use crate::gui::ApplicationWindow;
 use alloc::string::String;
 use dom::*;
 use liumlib::*;
 use tokenizer::*;
 
 #[allow(dead_code)]
-pub fn render(html: String) {
-    println!("===== rendering start ===== ");
-    println!("{}", html);
+pub fn render(html: String, _window: &ApplicationWindow) {
+    println!("input html:\n{}", html);
 
     let t = Tokenizer::new(html);
     let _root = Parser::new(t).construct_tree();
