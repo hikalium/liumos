c8046641ae58218e53ccad8388773388f3e36bc0
Asami Doi <doiasami1219@gmail.com>
2021-12-31 20:03:54 +0900

    browser-rs: update a default UI

--- app/browser-rs/src/gui.rs
+++ app/browser-rs/src/gui.rs
@@ -25,9 +25,16 @@ impl ApplicationWindow {
     }
 
     pub fn initialize(&mut self) {
+        let component_margin = 2;
+        let window_margin = 4;
+
+        let button_width = 18;
+        let address_bar_height = 20;
+
+        // base window
         match draw_rect(
             &self.buffer,
-            0xffffff, // color (red, green, blue)
+            0xb8b8b8, // grey
             0,
             0,
             self.width as i64,
@@ -37,14 +44,14 @@ impl ApplicationWindow {
             Err(e) => panic!("Failed to draw a background window: {:?}", e),
         };
 
-        // address bar
+        // title bar
         match draw_rect(
             &self.buffer,
-            0x2222ff,              // color (red, green, blue)
-            1,                     // px
-            1,                     // py
-            self.width as i64 - 2, // w
-            20,                    // h
+            0x000078,                              // blue
+            window_margin,                         // px
+            window_margin,                         // py
+            self.width as i64 - window_margin * 2, // w
+            address_bar_height,                    // h
         ) {
             Ok(()) => {}
             Err(e) => panic!("Failed to draw a bar: {:?}", e),
@@ -53,38 +60,69 @@ impl ApplicationWindow {
         // close button
         match draw_rect(
             &self.buffer,
-            0xff3333,               // color (red, green, blue)
-            self.width as i64 - 20, // px
-            3,                      // py
-            16,                     // w
-            16,                     // h
+            0xb8b8b8,                                                              // grey
+            self.width as i64 - (button_width + component_margin + window_margin), // px
+            window_margin + component_margin,                                      // py
+            button_width - component_margin,                                       // w
+            button_width - component_margin,                                       // h
         ) {
             Ok(()) => {}
             Err(e) => panic!("Failed to draw a close button: {:?}", e),
         };
 
+        /*
+        // close button (\)
         match draw_line(
             &self.buffer,
-            0xffffff,               // color
-            self.width as i64 - 20, // x0
-            3,                      // y0
-            self.width as i64 - 5,  // x1
-            18,                     // y1
+            0x000000,                                                                // black
+            self.width as i64 - window_margin - component_margin - button_width + 2, // px
+            window_margin + component_margin + 2,                                    // py
+            self.width as i64 - window_margin - component_margin - 5,                // x1
+            window_margin + component_margin + button_width - 3,                     // y1
         ) {
             Ok(()) => {}
             Err(e) => panic!("Failed to draw a line: {:?}", e),
         };
 
+        // close button (/)
         match draw_line(
             &self.buffer,
-            0xffffff,               // color
-            self.width as i64 - 5,  // x0
-            3,                      // y0
-            self.width as i64 - 20, // x1
-            18,                     // y1
+            0x000000,                                                                // black
+            self.width as i64 - window_margin - component_margin - 5,                // px
+            window_margin + component_margin + 1,                                    // py
+            self.width as i64 - window_margin - component_margin - button_width + 2, // x1
+            window_margin + component_margin + button_width - 4,                     // y1
         ) {
             Ok(()) => {}
             Err(e) => panic!("Failed to draw a line: {:?}", e),
+        };
+        */
+
+        // address bar
+        match draw_rect(
+            &self.buffer,
+            0xffffff,                                              // white
+            60 + window_margin,                                    // px
+            window_margin + address_bar_height + component_margin, // py
+            self.width as i64 - (60 + window_margin * 2),          // w
+            address_bar_height,                                    // h
+        ) {
+            Ok(()) => {}
+            Err(e) => panic!("Failed to draw a bar: {:?}", e),
+        };
+
+        // content for html
+        match draw_rect(
+            &self.buffer,
+            0xffffff, // white
+            window_margin,
+            address_bar_height * 2 + window_margin + component_margin * 2,
+            self.width as i64 - window_margin * 2,
+            self.height as i64
+                - (address_bar_height * 2 + window_margin * 2 + component_margin * 2),
+        ) {
+            Ok(()) => {}
+            Err(e) => panic!("Failed to draw a background window: {:?}", e),
         };
 
         self.buffer.flush();
